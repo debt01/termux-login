96e48dc134271760eaf9edc48b983fafab457e55
Linux Hunter420 <invention013654@gmail.com>
2025-07-11 21:40:40 +0600

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -1,5 +1,10 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
+# Copyright (C) 2024 [Linux HUnter420] - All Rights Reserved
+# Unauthorized copying/modification prohibited
+# License: See LICENSE.md file
+# Copyright (C) 2025 [debt01] - All Rights Reserved
+# Unauthorized copying/modification prohibited
+# License: See LICENSE.md file
 # TermuX-Custom Banner
 echo -e "\033[1;32m"
 echo " ████████╗███████╗██████╗ ███╗   ███╗██╗   ██╗██╗  ██╗"
